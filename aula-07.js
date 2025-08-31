c7839d9238c3fcaad03dbe689971cabe44771960
Eliabe Gabriel <eliabe.telematica@gmail.com>
2025-08-30 21:10:06 -0300

    feat: Aula-07 Array

---
+++ aula-07.js
@@ -0,0 +1,13 @@
+// Array:
+const idade = 18;
+
+const idades = [18, 14, 53, 56, 58, 45, 26, 79, 12, 48, 46];
+
+console.log(idades)
+console.log(idades.length)
+console.log(idades)
+
+for(let i = 0; i < idades.length; i++){
+    console.log(idades[i]);
+    i++;
+}
