643b908f14bbdc05c7c1bdfd8fbe33b629bc0cdf
eliabe <eliabe.telematica@gmail.com>
2025-09-01 17:37:17 -0300

    feat: aula09: Função

---
+++ aula-09.js
@@ -0,0 +1,22 @@
+// função:
+
+soma(5, 5);
+subtracao(5, 5);
+multiplicacao(5, 5);
+divisao(5, 5);
+
+function soma(num1, num2) {
+  console.log("SOMA = ", num1 + num2);
+}
+
+function subtracao(num1, num2) {
+  console.log("SUBTRAÇÃO = ", num1 - num2);
+}
+
+function multiplicacao(num1, num2) {
+  console.log("MULTIPLICAÇÃO = ", num1 * num2);
+}
+
+function divisao(num1, num2) {
+  console.log("DIVISÃO = ", num1 / num2);
+}
