8802155a468d9ff92ee21371f20113d8f277a689
Eliabe Gabriel <eliabe.telematica@gmail.com>
2025-09-02 09:31:59 -0300

    Operadores

---
+++ aula-11.js
@@ -0,0 +1,16 @@
+//Operadores: 
+// + Soma
+// - Subtração
+// * Multiplicação
+// / Divisão
+
+// % Resto
+// ** Elevado
+
+console.log("10 + 2 = ", 10 + 2)
+console.log("10 - 2 = ", 10 - 2)
+console.log("10 * 2 = ", 10 * 2)
+console.log("10 / 2 = ", 10 / 2)
+console.log("10 % 2 = ", 10 % 2)
+console.log("10 ** 2 = ", 10 ** 2)
+
