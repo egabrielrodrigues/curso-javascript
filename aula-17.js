9ae6d6ba7d7a262738ad17b1ea02dfd590f25644
eliabe <eliabe.telematica@gmail.com>
2025-09-03 14:52:22 -0300

    switch

--- aula-17.js
+++ aula-17.js
@@ -17,4 +17,34 @@ function estado(estado) {
   }
 }
 
-console.log(estado("sp"));
+// console.log(estado("sp"));
+
+// // Função que recebe o nome do estado como parâmetro
+// function estado(estado) {
+//   // Estrutura switch → compara a variável com vários casos
+//   switch (estado) {
+//     // Caso o valor seja "SP" ou "sp"
+//     case "SP":
+//     case "sp":
+//       console.log("São Paulo"); // Exibe "São Paulo"
+//       break; // Interrompe o switch, não executa os próximos casos
+
+//     // Caso o valor seja "RJ"
+//     case "RJ":
+//       console.log("Rio de Janeiro");
+//       break;
+
+//     // Caso o valor seja "PE"
+//     case "PE":
+//       console.log("Pernambuco");
+//       break;
+
+//     // Caso nenhum dos anteriores seja satisfeito
+//     default:
+//       console.log("estado Não encontrado");
+//       break;
+//   }
+// }
+
+// // Chama a função passando "sp" como argumento
+// console.log(estado("sp")); // Exibe "São Paulo" e undefined, pois a função não retorna nada
