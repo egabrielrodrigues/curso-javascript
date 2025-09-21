a909822cdd45e0331693c5fa8ac3f5a438144fa5
eliabe <eliabe.telematica@gmail.com>
2025-09-21 09:16:23 -0300

    async await

--- aula-22.js
+++ aula-22.js
@@ -1,33 +1,32 @@
 // Função Async Await
 
-
 const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 const aguardarComRetorno = async () => {
-    await sleep(3000);
-    return 30;
-}
+  await sleep(3000);
+  return 30;
+};
 
 const aguardar = async () => {
-    console.log('PASSOU', 2 ** 2);
-    const resultado = await aguardarComRetorno();
-    console.log('resultado', resultado)
-    await sleep(3000); //espera por 3 seg
-    // setTimeout(() => {
-    //     console.log("PASSOU TIMEOUT")
-    // }, 1000);
-    console.log('PASSOU', 3 ** 3);
-    console.log('PASSOU', 4 ** 4);
-    console.log('PASSOU', 5 ** 5);
-}
+  console.log("PASSOU", 2 ** 2);
+  const resultado = await aguardarComRetorno();
+  console.log("resultado", resultado);
+  await sleep(3000); //espera por 3 seg
+  // setTimeout(() => {
+  //     console.log("PASSOU TIMEOUT")
+  // }, 1000);
+  console.log("PASSOU", 3 ** 3);
+  console.log("PASSOU", 4 ** 4);
+  console.log("PASSOU", 5 ** 5);
+};
 
 aguardar();
 
 //---------------------------------------------------------------------
 
-// Quando você coloca a palavra async antes de uma função, 
+// Quando você coloca a palavra async antes de uma função,
 // automaticamente ela passa a retornar uma Promise.
 
 async function exemplo() {
@@ -35,7 +34,7 @@ async function exemplo() {
 }
 exemplo().then(console.log); // imprime 42
 
-// Ou seja: mesmo que você não escreva Promise, 
+// Ou seja: mesmo que você não escreva Promise,
 // o async já encapsula o retorno dentro de uma.
 
 //---------------------------------------------------------------------
@@ -45,7 +44,7 @@ exemplo().then(console.log); // imprime 42
 // Dentro de uma função marcada como async, você pode usar await.
 // Ele serve para pausar a execução até que uma Promise termine.
 
-const esperar = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const esperar = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function teste() {
   console.log("Antes");
@@ -53,5 +52,24 @@ async function teste() {
   console.log("Depois");
 }
 
-// É como se fosse “sincrônico”, mas na prática continua 
+// É como se fosse “sincrônico”, mas na prática continua
 // sendo assíncrono (não trava o navegador inteiro, só aquela função).
+
+function primeirFuncao() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Esperou isso aqui");
+      resolve();
+    }, 1000);
+  });
+}
+
+async function segundaFuncao() {
+  console.log("iniciou");
+
+  await primeirFuncao();
+
+  console.log("terminou");
+}
+
+segundaFuncao();
